03b64b6555584c1a66174fe59479caba7811fdcb
Daniela Araújo <dana.araujo@gmail.com>
2017-09-17 15:44:00 -0300

    Creates forecast adapter

--- app/src/main/java/com/nyan/weatherapp/MainActivity.kt
+++ app/src/main/java/com/nyan/weatherapp/MainActivity.kt
@@ -1,14 +1,28 @@
 package com.nyan.weatherapp
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private val items = listOf(
+        "Mon 6/23 - Sunny - 31/17",
+        "Mon 6/23 - Sunny - 31/17",
+        "Mon 6/23 - Sunny - 31/17",
+        "Mon 6/23 - Sunny - 31/17",
+        "Mon 6/23 - Sunny - 31/17",
+        "Mon 6/23 - Sunny - 31/17",
+        "Mon 6/23 - Sunny - 31/17"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        message.text = "Hello, kotlin!"
+
+        val forecastList = findViewById<RecyclerView>(R.id.forecast_list)
+        forecastList.layoutManager = LinearLayoutManager(this)
+        forecastList.adapter = ForecastListAdapter(items)
     }
 }
