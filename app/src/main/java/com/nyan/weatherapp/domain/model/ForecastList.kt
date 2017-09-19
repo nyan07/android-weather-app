834dddc5f33821cae437a3bca57b21d72644f1cb
Daniela Araújo <dana.araujo@gmail.com>
2017-09-19 20:12:33 -0300

    Operator overloading

--- app/src/main/java/com/nyan/weatherapp/domain/model/ForecastList.kt
+++ app/src/main/java/com/nyan/weatherapp/domain/model/ForecastList.kt
@@ -1,3 +1,8 @@
 package com.nyan.weatherapp.domain.model
 
-data class ForecastList(val city:String, val country:String, val dailyForecast:List<Forecast>)
+data class ForecastList(val city:String, val country:String, private val dailyForecast:List<Forecast>){
+    val size:Int
+        get() = dailyForecast.size
+
+    operator fun get(position:Int):Forecast = dailyForecast[position]
+}
