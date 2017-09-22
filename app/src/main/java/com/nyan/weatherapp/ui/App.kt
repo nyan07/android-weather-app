665d16fbb3a29e1b385cb34507f22fa0afc575a2
Daniela Araújo <dana.araujo@gmail.com>
2017-09-21 23:15:21 -0300

    Application singleton and delegated properties

---
+++ app/src/main/java/com/nyan/weatherapp/ui/App.kt
@@ -0,0 +1,17 @@
+package com.nyan.weatherapp.ui
+
+import android.app.Application
+import com.nyan.weatherapp.ui.utils.DelegatesExt
+
+class App : Application() {
+
+    companion object {
+        var instance: App by DelegatesExt.notNullSingleValue()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+
+}
