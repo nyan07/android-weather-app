f7c98cf357bc17e8ddfd73310f7473ffc0a92019
Daniela Araújo <dana.araujo@gmail.com>
2017-09-23 11:47:00 -0300

    Creating an SQLiteOpenHelper

---
+++ app/src/main/java/com/nyan/weatherapp/data/db/CityForecastTable.kt
@@ -0,0 +1,8 @@
+package com.nyan.weatherapp.data.db
+
+object CityForecastTable {
+    val NAME = "CityForecast"
+    val ID = "_id"
+    val CITY = "city"
+    val COUNTRY = "country"
+}
---
+++ app/src/main/java/com/nyan/weatherapp/data/db/DayForecastTable.kt
@@ -0,0 +1,12 @@
+package com.nyan.weatherapp.data.db
+
+object DayForecastTable {
+    val NAME = "DayForecast"
+    val ID = "_id"
+    val DATE = "date"
+    val DESCRIPTION = "description"
+    val HIGH = "high"
+    val LOW = "low"
+    val ICON_URL = "iconUrl"
+    val CITY_ID = "cityId"
+}
