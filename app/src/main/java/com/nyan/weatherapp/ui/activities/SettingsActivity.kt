5e91b695ad6002f618be4a84c23c635a4819b497
Daniela Araújo <dana.araujo@gmail.com>
2017-09-30 19:56:04 -0300

    Creating the settings activity

---
+++ app/src/main/java/com/nyan/weatherapp/ui/activities/SettingsActivity.kt
@@ -0,0 +1,26 @@
+package com.nyan.weatherapp.ui.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import com.nyan.weatherapp.R
+
+import kotlinx.android.synthetic.main.toolbar.*
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?) = when (item?.itemId) {
+        android.R.id.home -> {
+            onBackPressed()
+            true
+        }
+        else -> false
+    }
+}
